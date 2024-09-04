a5b4e81cfd309ac881af6105bb9eb6c7879462f0
nrkhan-223 <redoykhan8141@gmail.com>
2024-09-04 12:04:41 +0600

    compass_screen change

--- lib/screen/compass_screen.dart
+++ lib/screen/compass_screen.dart
@@ -95,19 +95,19 @@ class CenterDisplay extends StatelessWidget {
     String getDirection(double direction) {
       if (direction >= 337.5 || direction < 22.5) {
         return 'N';
-      } else if (direction >= 22.5 || direction < 112.5) {
+      } else if (direction >= 22.5 && direction < 67.5) {
         return 'NE';
-      } else if (direction >= 22.5 || direction < 112.5) {
-        return 'NE';
-      } else if (direction >= 112.5 || direction < 157.5) {
+      } else if (direction >= 67.5 && direction < 112.5) {
         return 'E';
-      } else if (direction >= 157.5 || direction < 202.5) {
+      } else if (direction >= 112.5 && direction < 157.5) {
+        return 'SE';
+      } else if (direction >= 157.5 && direction < 202.5) {
         return 'S';
-      } else if (direction >= 202.5 || direction < 247.5) {
+      } else if (direction >= 202.5 && direction < 247.5) {
         return 'SW';
-      } else if (direction >= 247.5 || direction < 297.5) {
+      } else if (direction >= 247.5 && direction < 297.5) {
         return 'W';
-      } else if (direction >= 297.5 || direction < 337.5) {
+      } else if (direction >= 297.5 && direction < 337.5) {
         return 'NW';
       }
       return 'N';
